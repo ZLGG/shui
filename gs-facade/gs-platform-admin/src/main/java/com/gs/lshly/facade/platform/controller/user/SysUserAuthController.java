a9836fa499b90e8f82a05a677661849a360481ac
yingjun <yingj@citydo.com.cn>
2021-05-19 13:08:22 +0800

    登录帐号

--- gs-facade/gs-platform-admin/src/main/java/com/gs/lshly/facade/platform/controller/user/SysUserAuthController.java
+++ gs-facade/gs-platform-admin/src/main/java/com/gs/lshly/facade/platform/controller/user/SysUserAuthController.java
@@ -105,6 +105,7 @@ public class SysUserAuthController {
         	 * 解码
         	 */
         	String password = AES.Encrypt(dto.getPassword());
+        	dto.setPassword(password);
         	SysUserVO.DetailVO userDetails = sysUserRpc.getSysUserByName(dto.getUsername());
             if (userDetails == null || StringUtils.isEmpty(userDetails.getId())) {
                 throw new BusinessException("找不到该用户");
