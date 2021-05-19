3a4921c6fe65f41a273993b5ebce8295a7307988
yangxi <yangxi@citydo.com.cn>
2021-05-19 20:08:11 +0800

    修复类目bug

--- gs-biz-support/gs-foundation/gs-foundation-srv/src/main/java/com/gs/lshly/biz/support/foundation/service/bbc/impl/BbcPointHomeServiceImpl.java
+++ gs-biz-support/gs-foundation/gs-foundation-srv/src/main/java/com/gs/lshly/biz/support/foundation/service/bbc/impl/BbcPointHomeServiceImpl.java
@@ -87,8 +87,8 @@ public class BbcPointHomeServiceImpl implements IBbcPointHomeService {
 		listVO.setCode(PointHomeTypeEnum.分类.getCode());
 		listVO.setIdx(PointHomeTypeEnum.分类.getIdx());
 		listVO.setName(PointHomeTypeEnum.分类.getRemark());
-		listVO.setList(goodsCategoryRpc.selectCategoryTree());
-//		listVO.setList(goodsCategoryRpc.selectCategoryTreeWithGoods());
+//		listVO.setList(goodsCategoryRpc.selectCategoryTree());
+		listVO.setList(goodsCategoryRpc.selectCategoryTreeWithGoods());
 		retList.add(listVO);
 		
 		PCBbbGoodsCategoryQTO.QTO qto1 = new PCBbbGoodsCategoryQTO.QTO();
