b6c5d9783e29c3e75c485e8f5b8b887994af1ce5
yingjun <yingj@citydo.com.cn>
2021-05-27 21:23:47 +0800

    不能购买的原因

--- gs-biz-support/gs-commodity/gs-commodity-srv/src/main/java/com/gs/lshly/biz/support/commodity/service/bbc/impl/BbcGoodsInfoServiceImpl.java
+++ gs-biz-support/gs-commodity/gs-commodity-srv/src/main/java/com/gs/lshly/biz/support/commodity/service/bbc/impl/BbcGoodsInfoServiceImpl.java
@@ -380,14 +380,18 @@ public class BbcGoodsInfoServiceImpl implements IBbcGoodsInfoService {
 
         BbcSkuGoodInfoVO.SkuVO skuVO = getSkuList(goodsInfo).get(0);
         //若是多规格填充默认规格信息
-//        if (goodsInfo.getIsSingle().intValue() == SingleStateEnum.多规格.getCode().intValue()) {
         //获取默认规格商品信息
         detailVo.setSkuVO(skuVO);
 
         detailVo.setSkuId(skuVO.getSkuId());
 
         detailVo.setSalePrice(skuVO.getSkuSalePrice());
-//        }
+        if (goodsInfo.getIsSingle().intValue() == SingleStateEnum.单品.getCode().intValue()) {
+        	if(detailVo.getIsBuy().equals(0)){
+	        	detailVo.setIsBuy(skuVO.getIsBuy());
+	        	detailVo.setBuyRemark(skuVO.getBuyRemark());
+	        }
+        }
 
         //填充spec规格列表信息
         if (ObjectUtils.isNotEmpty(getSpecInfoVO(goodsInfo))) {
@@ -1074,6 +1078,17 @@ public class BbcGoodsInfoServiceImpl implements IBbcGoodsInfoService {
                 skuVO.setSkuSalePrice(skuGoodInfo.getSalePrice());
                 skuVO.setSpecValue(StringUtils.isEmpty(skuGoodInfo.getSpecsValue()) ? "" : skuGoodInfo.getSpecsValue());
                 skuVO.setImage(StringUtils.isBlank(skuGoodInfo.getImage()) ? "" : skuGoodInfo.getImage());
+                
+                Integer quantity = bbcStockRpc.getQuantity(skuGoodInfo.getId());
+                if(quantity==null||quantity.equals(0)){
+                	skuVO.setIsBuy(0);
+                	skuVO.setBuyRemark(GoodsBuyRemarkEnum.库存不足.getRemark());
+                }
+                
+                if(!(skuGoodInfo.getState()).equals(GoodsStateEnum.已上架.getCode())){
+                	skuVO.setIsBuy(0);
+                	skuVO.setBuyRemark(GoodsBuyRemarkEnum.getRemark(skuGoodInfo.getState()));
+                }
             }
             skuVOS.add(skuVO);
         }
