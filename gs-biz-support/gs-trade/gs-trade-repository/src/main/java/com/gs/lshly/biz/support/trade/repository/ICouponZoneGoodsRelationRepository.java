6663152c097dc79a19df3e15ec060d99fa380c7f
yang chen <cy082611@126.com>
2021-05-31 09:32:34 +0800

    1.优惠券接口修改

---
+++ gs-biz-support/gs-trade/gs-trade-repository/src/main/java/com/gs/lshly/biz/support/trade/repository/ICouponZoneGoodsRelationRepository.java
@@ -0,0 +1,16 @@
+package com.gs.lshly.biz.support.trade.repository;
+
+import com.gs.lshly.biz.support.trade.entity.CouponZoneGoodsRelation;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 优惠券专区黑名单商品关联表 服务类
+ * </p>
+ *
+ * @author chenyang
+ * @since 2021-05-28
+ */
+public interface ICouponZoneGoodsRelationRepository extends IService<CouponZoneGoodsRelation> {
+
+}
