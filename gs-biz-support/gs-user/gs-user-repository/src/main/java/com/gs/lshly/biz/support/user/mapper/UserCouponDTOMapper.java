58cf8e9fa2883ef817a64a7829f41465f5b232c1
yingjun <yingj@citydo.com.cn>
2021-07-08 14:23:43 +0800

    取消时间控制

--- gs-biz-support/gs-user/gs-user-repository/src/main/java/com/gs/lshly/biz/support/user/mapper/UserCouponDTOMapper.java
+++ gs-biz-support/gs-user/gs-user-repository/src/main/java/com/gs/lshly/biz/support/user/mapper/UserCouponDTOMapper.java
@@ -22,6 +22,6 @@ public interface UserCouponDTOMapper extends BaseMapper<UserCoupon> {
     @Select("select count(1) from gs_goods_info where is_in_member_gift = 1 and id = #{goodsId}")
     Integer isInGoods(@Param("goodsId") String goodsId);
     
-    @Select("select * from gs_user_coupon where coupon_id ='${couponId}' and user_id='${userId}' and start_time<=NOW() and end_time>=NOW() and coupon_status = 0 ")
+    @Select("select * from gs_user_coupon where coupon_id ='${couponId}' and user_id='${userId}' and coupon_status = 0 ")
     List<BbcUserCouponVO.ListVO> listByCouponId(@Param("couponId") String couponId,@Param("userId")String userId);
 }
