a2ac4713a7e5cfa935b812a52974bb37c7b138ba
“yusq” <yusq@citydo.com.cn>
2021-07-14 14:03:33 +0800

    修改短信发送为远程调用

--- gs-middleware/gs-sms/src/main/java/com/gs/lshly/middleware/sms/IContactSMSService.java
+++ gs-middleware/gs-sms/src/main/java/com/gs/lshly/middleware/sms/IContactSMSService.java
@@ -1,5 +1,6 @@
 package com.gs.lshly.middleware.sms;
 
+import java.io.IOException;
 import java.util.Map;
 
 public interface IContactSMSService  {
@@ -10,7 +11,7 @@ public interface IContactSMSService  {
      * @param phone
      * @return java.util.Map<java.lang.String,java.lang.String>
      */
-    Map<String, String> userLogin(String phone);
+    String userLogin(String phone) throws IOException;
     /**
      *功能描述  商户登录
      * @author yushaoqi
@@ -18,7 +19,7 @@ public interface IContactSMSService  {
      * @param phone
      * @return java.util.Map<java.lang.String,java.lang.String>
      */
-    Map<String, String> merchantLogin(String phone);
+    String merchantLogin(String phone);
     /**
      *功能描述 支付验证码
      * @author yushaoqi
@@ -26,7 +27,7 @@ public interface IContactSMSService  {
      * @param phone
      * @return java.util.Map<java.lang.String,java.lang.String>
      */
-    Map<String, String> payment(String phone);
+    String payment(String phone);
     /**
      *功能描述 下单成功提醒
      * @author yushaoqi
@@ -37,7 +38,7 @@ public interface IContactSMSService  {
      * @param balancePoint  积分余额
      * @return java.util.Map<java.lang.String,java.lang.String>
      */
-    Map<String, String> reminderSuccessfulOrder(String phone,String tradeCode,String point,String balancePoint);
+    String reminderSuccessfulOrder(String phone,String tradeCode,String point,String balancePoint);
 
     /**
      *功能描述 已发货提醒
@@ -49,7 +50,7 @@ public interface IContactSMSService  {
      * @param deliveryCode  快递单号
      * @return java.util.Map<java.lang.String,java.lang.String>
      */
-    Map<String, String> shipmentReminder(String phone,String userName, String deliveryCompany,String deliveryCode);
+    String shipmentReminder(String phone,String userName, String deliveryCompany,String deliveryCode);
 
     /**
      *功能描述 已签收短信
@@ -61,7 +62,7 @@ public interface IContactSMSService  {
      * @param deliveryCode 快递单号
      * @return java.util.Map<java.lang.String,java.lang.String>
      */
-    Map<String, String> signForSMS(String phone,String userName, String deliveryCompany,String deliveryCode);
+    String signForSMS(String phone,String userName, String deliveryCompany,String deliveryCode);
 
     /**
      *功能描述  售后服务提醒
@@ -71,7 +72,7 @@ public interface IContactSMSService  {
      * @param userName 用户名称
      * @return java.util.Map<java.lang.String,java.lang.String>
      */
-    Map<String, String> afterSalesServiceReminder(String phone,String userName);
+    String afterSalesServiceReminder(String phone,String userName);
     /**
      *功能描述 退款提醒
      * @author yushaoqi
@@ -80,7 +81,7 @@ public interface IContactSMSService  {
      * @param userName 用户名称
      * @return java.util.Map<java.lang.String,java.lang.String>
      */
-    Map<String, String> refundReminder(String phone,String userName);
+    String refundReminder(String phone,String userName);
 
     /**
      *功能描述  优惠券发放提醒
@@ -91,6 +92,6 @@ public interface IContactSMSService  {
      * @param loginUrl  登录url
      * @return java.util.Map<java.lang.String,java.lang.String>
      */
-    Map<String, String> couponIssuanceReminder(String phone,String endDate,String loginUrl);
+    String couponIssuanceReminder(String phone,String endDate,String loginUrl);
 
 }
